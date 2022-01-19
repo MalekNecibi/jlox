08b85d8bd07fd748b547aa02c27ba0c825a133d6
Malek Necibi <Malek.Necibi@vu.com>
2022-01-19 13:20:14 -0600

    added package, tokens enum

--- lox/us/necibi/lox/Lox.java
+++ lox/us/necibi/lox/Lox.java
@@ -1,3 +1,5 @@
+package us.necibi.lox;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,6 +7,29 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
+
+// convert common tokens to numbers. avoids expensive/repetitive string evaluation
+enum TokenType {
+    // Single-Char tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One/Two Char Tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
 
 public class Lox {
     static boolean hadError = false;
